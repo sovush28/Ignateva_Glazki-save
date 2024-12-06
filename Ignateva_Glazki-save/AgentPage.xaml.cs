ec4a00dcba3576ce148c04969dd8065a9da7d9b2
Yulia Ignateva <yuliya.ignateva.06@mail.ru>
2024-12-06 13:43:58 +0500

    Добавьте файлы проекта.

---
+++ Ignateva_Glazki-save/AgentPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ignateva_Glazki_save
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentPage.xaml
+    /// </summary>
+    public partial class AgentPage : Page
+    {
+        public AgentPage()
+        {
+            InitializeComponent();
+
+            var currentAgents = ИгнатьеваГлазкиSaveEntities.GetContext().Agent.ToList();
+            var currentAgentTypes = ИгнатьеваГлазкиSaveEntities.GetContext().AgentType.ToList();
+            AgentListView.ItemsSource = currentAgents;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage());
+        }
+    }
+}
